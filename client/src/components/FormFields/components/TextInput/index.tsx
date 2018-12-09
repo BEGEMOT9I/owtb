9e293ff26a1c23fc0c49ea5276a31a73267a548c
BEGEMOT9I <daston2012@gmail.com>
2018-12-10 00:40:24 +0300

    removed debouncing from text field for fixing error

--- client/src/components/FormFields/components/TextInput/index.tsx
+++ client/src/components/FormFields/components/TextInput/index.tsx
@@ -14,47 +14,9 @@ interface OuterProps extends InputHTMLAttributes<HTMLInputElement>, FieldRenderP
   withoutDelay?: boolean
 }
 export interface Props extends OuterProps, JSSProps<typeof styles> {}
-interface State {
-  value: string
-}
+interface State {}
 
 class TextInput extends PureComponent<Props, State> {
-  timeout: NodeJS.Timer
-
-  constructor(props: Props) {
-    super(props)
-
-    this.state = {
-      value: props.input.value
-    }
-  }
-
-  componentWillReceiveProps(nextProps: Props) {
-    if (nextProps.input.value !== this.props.input.value) {
-      this.setState({ value: nextProps.input.value })
-    }
-  }
-
-  onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const {
-      input: { onChange },
-      withoutDelay
-    } = this.props
-    const value = event.target.value
-
-    this.setState({ value })
-
-    if (withoutDelay) {
-      onChange(value)
-    } else {
-      if (this.timeout) {
-        clearTimeout(this.timeout)
-      }
-
-      this.timeout = setTimeout(() => onChange(value), 250)
-    }
-  }
-
   render() {
     const {
       classes,
@@ -65,28 +27,28 @@ class TextInput extends PureComponent<Props, State> {
       label,
       placeholder,
       type = 'text',
-      input: { onChange, value, ...input },
+      input: { name, value, onChange, ...input },
       meta: { error, touched },
       disabled
     } = this.props
-    console.log(input.name, touched, error, value)
+    console.log(name, touched, error, value)
 
     return (
       <div className={classNames(classes.container, className)}>
         <div className={classes.content}>
           {!!label && (
-            <label className={classes.label} htmlFor={id || input.name}>
+            <label className={classes.label} htmlFor={id || name}>
               {label}
             </label>
           )}
           <input
-            id={id || input.name}
+            id={id || name}
             type={type}
             placeholder={placeholder}
             className={classNames(classes.input, inputClassName)}
-            value={this.state.value}
+            value={value}
             disabled={disabled}
-            onChange={this.onChange}
+            onChange={onChange}
             {...input}
           />
         </div>
